3f37a0e8f4c841d1e53afad7b55f3bbcc69708d9
Vepnore <46622977+Vepnore@users.noreply.github.com>
2019-10-10 14:27:14 +0800

    合并文件

--- Function.py
+++ Function.py
@@ -1,8 +1,9 @@
 import os.path
 import datetime
+import json
 
 # 图片存储位置
-_folder_name = 'H:/Yandere/'
+_folder_name = 'K:/Yandere/'
 
 
 def create_folder():
@@ -51,7 +52,7 @@ def get(file_name: str):
     :return: str
     """
     file = open(file_name)
-    data = file.readline()
+    data = file.read()
     file.close()
     return data
 
@@ -74,3 +75,28 @@ def char_replace(file_name):
     for chr in chr_list:
         file_name = file_name.replace(chr, '')
     return file_name
+
+
+def read_settings(json_file):
+    return json.loads(json_file)
+
+
+def get_settings(settings: dict):
+    settings['start_page'] = int(input('开始页码：'))
+    settings['stop_page'] = int(input('停止页码，为0时爬取至上次终止图片，非0时爬完此页即停止：'))
+    settings['pic_type'] = int(input('图片比例，0=全部 1=横图 2=竖图 3=正方形：'))
+    settings['pic_ext'] = input('图片类型，png/jpg：')
+
+    print('输入图片尺寸限制条件，为0则不限制：')
+    settings['pic_size']['min']['width'] = int(input('最小宽度：'))
+    settings['pic_size']['min']['height'] = int(input('最小高度：'))
+    settings['pic_size']['min']['proportion'] = int(input('最小宽高比：'))
+    settings['pic_size']['max']['width'] = int(input('最大宽度：'))
+    settings['pic_size']['max']['height'] = int(input('最大高度：'))
+    settings['pic_size']['max']['proportion'] = int(input('最大宽高比：'))
+    return settings
+
+
+def edit_settings(settings: dict, item, value: int):
+    settings[item] = value
+    return settings
